fe355756beb9b52e57cb1d50c2fd2fa72be8a9cc
ibtg <ibtg0409@gmail.com>
2021-03-05 17:10:29 +0900

    Delete files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 import vuetify from './plugins/vuetify';
 import lightbox from 'vlightbox';
 
@@ -11,8 +9,6 @@ Vue.use(lightbox);
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
